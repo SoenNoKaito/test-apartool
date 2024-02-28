c154be9807067aab41358355e01c9a09d1ab2ebe
SoenNoKaito <jcdevstorm@gmail.com>
2024-02-28 21:14:04 +0100

    - Create a login route in order to get token

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error', $validator->errors());
+        }
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            $success['name'] = $user->name;
+
+            return $this->sendResponse($success, 'User login successful.');
+        } else {
+            // No need to manually manage login attempts, the throttle middleware will handle it
+            return $this->sendError('Unauthorized', ['error' => 'Invalid credentials'], 401);
+        }
+    }
+}
